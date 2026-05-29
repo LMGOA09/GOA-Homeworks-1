a8064edf1bfa0a251fe4bb8a01d78585f5b57d04
LMGOA09 <lukamotiashvili63@gmail.com>
2026-05-29 18:32:57 +0400

    first commit

---
+++ Homework9.py
@@ -0,0 +1,54 @@
+#1)
+secret = 7
+num = 0
+
+while num != secret:
+    num = int(input("Guess the secret number: "))
+
+print("you win")
+
+#2)
+a = 0
+b = 20
+nums = []
+
+while a < b:
+    a = a + 1
+    nums.append(a)
+
+print(nums)
+
+#3)
+age = 0
+
+while age < 18:
+    age = int(input("Enter your age: "))
+
+print("you are an adult")
+
+#4)
+num = 2
+while num <= 20:
+    print(num)
+    num = num + 2
+
+#5)
+num = 1
+while num <= 20:
+    print(num)
+    num = num + 2
+
+#6)
+# A flowchart is a visual diagram that shows the step-by-step steps of a process or a program. 
+# Instead of writing code, you use different geometric shapes connected by arrows to show how 
+# information flows and what decisions need to be made.
+
+#7)
+# Sequence is the simplest structure. It means the code runs line by line, from top to bottom, 
+# exactly in the order it was written. No skipping, no repeating.
+
+# Selection is where the program makes a decision. It uses conditions (like if statements) to choose 
+# which path of code to follow based on whether something is true or false.
+
+# Iteration is where the program makes a decision. It uses conditions (like if statements) to choose 
+# which path of code to follow based on whether something is true or false.
